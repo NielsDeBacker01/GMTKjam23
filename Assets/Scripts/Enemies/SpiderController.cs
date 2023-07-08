ee7b1bedb7dcbfdc9bacf45a58256712f249033e
AlaeChatar <alaechatar@gmail.com>
2023-07-08 17:44:12 +0200

    Implement enemy behaviour for spider

---
+++ Assets/Scripts/Enemies/SpiderController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+public class SpiderController : MonoBehaviour
+{
+    private bool isFacingRight = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= -10)
+            isFacingRight = false;
+        if (transform.position.x >= 10)
+            isFacingRight = true;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isFacingRight)
+            transform.position += new Vector3(-0.2f, 0f, 0f);
+        if (!isFacingRight)
+            transform.position += new Vector3(0.2f, 0f, 0f);
+    }
+
+
+}
