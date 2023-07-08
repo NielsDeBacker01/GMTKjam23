3415d6e547c960c069c2fd8ab8594810791b9b0a
AlaeChatar <alaechatar@gmail.com>
2023-07-08 22:53:21 +0200

    Link timer to game over scene

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
     private float counter;
-    private float timerValue = 59;
+    private float timerValue = 5;
 
     // Update is called once per frame
     void Update()
     {
         if (timerValue > 0)
-        {
             timerValue -= Time.deltaTime;
-        }
 
         timerText.text = "00:" + Mathf.Round(timerValue).ToString();
+
+        if (timerValue <= 0)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 }
