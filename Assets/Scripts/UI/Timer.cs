0306d1253a5c6b2524ecc3a6a7769bafd8add14f
AlaeChatar <alaechatar@gmail.com>
2023-07-09 12:08:34 +0200

    Add jitter effect to timer

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,19 +9,21 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
-    private float counter;
-    private float timerValue = 59;
+    private float timerValue = 12;
 
     // Update is called once per frame
     void Update()
     {
         if (timerValue > 0)
             timerValue -= Time.deltaTime;
+        
+        if (timerValue < 10)
+            timerText.text = "00:0" + Mathf.Round(timerValue).ToString();
+        else
+            timerText.text = "00:" + Mathf.Round(timerValue).ToString();
 
-        timerText.text = "00:" + Mathf.Round(timerValue).ToString();
 
         if (timerValue <= 0)
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
     }
 }
